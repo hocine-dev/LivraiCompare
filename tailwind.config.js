634d8a22cd8e166ad104bb5a91a40fbfbc1e65ef
HocineDev <hocinedev4@gmail.com>
2025-05-05 21:58:58 +0100

    version initiale

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: [
+    './templates/**/*.html.twig',
+    './assets/**/*.js',
+    './assets/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
